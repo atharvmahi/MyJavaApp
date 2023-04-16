86ac22a88f45e196277b9c1b645671e7ba97f3e3
atharvmahi <128790503+atharvmahi@users.noreply.github.com>
2023-04-16 15:53:12 +0530

    Update AppOne.java

--- AppOne.java
+++ AppOne.java
@@ -3,6 +3,7 @@ public class AppOne {
 
 	public static void main(String[] args) {
 		System.out.println("Mahesh Here for Devops");
+		System.out.println("Mahesh Here for Devops");
 		// TODO Auto-generated method stub
 
 	}
